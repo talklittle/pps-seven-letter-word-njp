ae40a93a2bf80eddcbe1de28180530b5a2deca3e
neetha.sebastian <neetha.sebastian@246d8b38-f93e-2632-be4d-0b2ae2cb7066>
2010-09-26 20:20:10 +0000

    Fix to change the bid levels from high to mid and mid to low if we already have that letter. (Eg : If we already have an E, and we were going to bid high, we will now bid mid.)

--- sevenLetterWord/src/seven/f10/g4/Bidder.java
+++ sevenLetterWord/src/seven/f10/g4/Bidder.java
@@ -3,6 +3,8 @@ package seven.f10.g4;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import org.apache.log4j.Logger;
+
 public class Bidder {
 	private static final String LOW = "LOW";
 	private static final String MID = "MID";
@@ -17,6 +19,7 @@ public class Bidder {
 	private HashMap<String, ArrayList<Character>> bidLevels;
 	private HashMap<Character,Integer> frequencyMap;
 	
+	private Logger logger = Logger.getLogger(G4Player.class);
 	
 	public void setBidLevels() {
 		bidLevels=new HashMap<String, ArrayList<Character>>();
@@ -25,6 +28,8 @@ public class Bidder {
 		ArrayList<Character> midBids=new ArrayList<Character>();
 		ArrayList<Character> lowBids=new ArrayList<Character>();
 		for(Character c: frequencyMap.keySet()){
+			
+			
 			if(2*frequencyMap.get(c)>=numberOfSevenLetterWords){
 				highBids.add(c);
 			}
@@ -39,8 +44,8 @@ public class Bidder {
 		bidLevels.put(MID, midBids);
 		bidLevels.put(LOW, lowBids);
 	}
-	
 
+	
 	public int getCompletingBid(int possiblePoints,int currentPoints) {
 		return possiblePoints - currentPoints ;
 	}
@@ -61,21 +66,34 @@ public class Bidder {
 
 	private float getBidMultiplier(Character targetCharacter) {
 		setBidLevels();
+		
+		float RETURN_BID;
 		if(bidLevels.get(HIGH).contains(targetCharacter)){
-			return HIGH_BID;
+			RETURN_BID = HIGH_BID;
 		}
-		if(bidLevels.get(MID).contains(targetCharacter)){
-			return MID_BID;
+		else if(bidLevels.get(MID).contains(targetCharacter)){
+			RETURN_BID = MID_BID;
 		}
-		return LOW_BID;
+		else {
+			RETURN_BID = LOW_BID;
+		}
+		
+		if(Util.getWordInRack()!= null && Util.getWordInRack().getWord().contains(targetCharacter.toString())) {
+			logger.debug("word in Rack" + Util.getWordInRack().getWord());
+			logger.debug("Bid level original" + RETURN_BID);
+			if(RETURN_BID == HIGH_BID) {
+				RETURN_BID = MID_BID;
+			} else if(RETURN_BID == MID_BID) {
+				RETURN_BID = LOW_BID;
+			}
+			logger.debug("Bid level changed" + RETURN_BID);
+		}
+		return RETURN_BID;
 	}
 	
 	private int getAverageSevenScore(Character targetCharacter, Status gameStatus) {
 		int sumSevenScore = 0;
 		int numSevenLetterWords = 0;
-		
-		
-		
 		return ASSUMED_WORD_SCORE;
 	}
 
